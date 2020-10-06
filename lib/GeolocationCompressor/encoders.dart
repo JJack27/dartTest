4737d3bd92a65963d3581a49d9d222985ad673af
jjack27 <yizhouzhao27@gmail.com>
2020-10-06 14:00:51 -0600

    add

--- lib/GeolocationCompressor/encoders.dart
+++ lib/GeolocationCompressor/encoders.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+import 'dart:core';
+
 /// Abstract class for encoders
 abstract class _BaseEncoder{
   List<int> encode(double value);
+  int _bitUsed;
 
   /// Convert decimal integer to binary format as list of integer
   ///
@@ -10,7 +14,7 @@ abstract class _BaseEncoder{
   ///   - List<int>
   /// throw:
   ///   - Exception
-  List<int> decimalToBinaryList(int value){
+  List<int> _decimalToBinaryList(int value){
     if(value < 0){
       throw Exception("Argument value has to be non-negative integer, but given $value.");
     }
@@ -22,20 +26,70 @@ abstract class _BaseEncoder{
     }
     return result;
   }
+
+  /// Degree to radian
+  /// args:
+  ///   - deg: double
+  double _degToRad(double deg){
+    return (deg * pi) / 180.0;
+  }
 }
 /// Encoder that encodes the latitude information
 class _LatitudeEncoder extends _BaseEncoder{
+
+
   @override
   List<int> encode(double value){
-
+    // determine flags
   }
 }
 
 /// Encoder that encodes the longitude information
 class _LongitudeEncoder extends _BaseEncoder{
+
+  // the degree of magnitude you want longitude to scale. The larger, the preciser.
+  int _deg = 7;
+
+  // The bit shift of flag that indicate if (pi/2 < longitude < 3pi/2)
+  int _overNinetyDegBitShifts = 24;
+
+  // The bit shift of flag indicate if longitude is negative
+  int _negBitShifts = 25;
+
+  /// Constructor
+  /// args:
+  ///   - deg: the degree of magnitude you want longitude to scale. The larger, the preciser.
+  _LongitudeEncoder({deg:7}){
+    this._deg = deg;
+
+    if(this._deg != 7){
+      // Get the maximum value of encoded longitude
+      int encodedUpperBound = pow(10, _deg);
+
+      // Calculate bits shift of flags: overNinetyDeg, negative
+      _overNinetyDegBitShifts = 0;
+      while(encodedUpperBound != 0){
+        encodedUpperBound = encodedUpperBound >> 1;
+        _overNinetyDegBitShifts++;
+      }
+      _negBitShifts = _overNinetyDegBitShifts + 1;
+    }
+
+    _bitUsed = _negBitShifts + 1;
+  }
+
   @override
   List<int> encode(double value){
+    // Determine flags
+    int negative = value >= 0 ? 0 : 1;
+    int overNinetyDeg = value.abs() > 90 ? 1 : 0;
 
+    // encoding the longitude
+    double longitude = sin(_degToRad(value));
+    int encodedLng = (longitude * (pow(10, _deg))).toInt().abs();
+    encodedLng = encodedLng | (negative << _negBitShifts) | (overNinetyDeg << _overNinetyDegBitShifts);
+
+    return _decimalToBinaryList(encodedLng);
   }
 }
 
@@ -57,6 +111,41 @@ class LocationTimeEncoder{
 }
 
 void main(){
-  _DatetimeEncoder datetimeEncoder = new _DatetimeEncoder();
-  print(datetimeEncoder.decimalToBinaryList(123));
+  List<double> lngTests = [0, 1, 45, 89, 90, 91, 100, 135, 179, 180];
+  List<double> lngTestsTemp = [];
+  List<List<int>> lngResult = [[0] ,
+      [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0] ,
+      [1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1] ,
+      [1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0] ,
+      [1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0] ,
+      [1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0] ,
+      [1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1] ,
+      [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1] ,
+      [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0] ,
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+      [0] ,
+      [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0] ,
+      [1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1] ,
+      [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0] ,
+      [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0] ,
+      [1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0] ,
+      [1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1] ,
+      [1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1] ,
+      [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0] ,
+      [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  for(double i in lngTests){
+    lngTestsTemp.add(-i);
+  }
+  lngTests = lngTests + lngTestsTemp;
+  _LongitudeEncoder lngEncoder = new _LongitudeEncoder();
+
+  for(int i = 0; i < lngTests.length; i++){
+    assert(lngEncoder.encode(lngTests[i]) == lngResult[i],
+      lngTests[i].toString() + ' is wrong');
+
+  }
+  print('Pass');
+
+
 }
